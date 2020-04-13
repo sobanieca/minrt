c9b48da5ad41380a57bc6029f810c8898f9226ac
adam.sobaniec <sobanieca@gmail.com>
2020-04-13 15:07:27 +0200

    lint fixes

--- test/router.test.js
+++ test/router.test.js
@@ -1,25 +1,24 @@
 import assert from "assert";
 let sinon;
 
-describe("given router", function() {
-    let router;
+describe("given router", function () {
+  let router;
 
-    let importCounter = 0;
-    beforeEach(async function() {
-        let routerModule = await import(`../lib/router.js?${importCounter}`); 
-        router = routerModule.default;
+  let importCounter = 0;
+  beforeEach(async function () {
+    let routerModule = await import(`../lib/router.js?${importCounter}`);
+    router = routerModule.default;
 
-        let sinonModule = await import("sinon");
-        sinon = sinonModule.default;
+    let sinonModule = await import("sinon");
+    sinon = sinonModule.default;
+  });
+
+  describe("when something", function () {
+    it("should do something", function () {
+      sinon.fake();
+      router.navigate("/somewhere");
+
+      assert.equal(1, 1);
     });
-
-    describe("when something", function() {
-        it("should do something", function() {
-            sinon.fake();
-            router.navigate('/somewhere');
-
-            assert.equal(1, 1);
-        });
-    });
-
+  });
 });
